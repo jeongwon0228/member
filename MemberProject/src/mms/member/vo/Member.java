ce3076dd2e1a206b45945ed4e4cf46ce47aa733b
jeongwon0228 <jaong7028@naver.com>
2025-02-14 17:40:21 +0900

    Add files via upload

---
+++ MemberProject/src/mms/member/vo/Member.java
@@ -0,0 +1,16 @@
+package mms.member.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Member {
+	private int id;
+	private String name;
+	private	String addr;
+	private	String nation;
+	private String email;
+	private int age;
+	
+}
